d31eafdc9f749317e2a5bba07261345f2963f344
mgafsia <mohamed.gafsia@gmail.com>
2020-03-24 01:33:38 +0100

    Add Map / MergeAll / MergeMap

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { of, from, timer, interval } from 'rxjs';
-import { map, take, delay } from 'rxjs/operators';
+import { map, take, delay, mergeAll, switchMap, mergeMap } from 'rxjs/operators';
+
+export interface Car {
+  brand: string;
+  model: string;
+  price: number;
+}
 
 @Component({
   selector: 'my-app',
@@ -8,53 +14,67 @@ import { map, take, delay } from 'rxjs/operators';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent implements OnInit  {  
-    cars = [
+    cars: Car[] = [
       {
         brand: 'porsche',
-        model: '911'
+        model: '911',
+        price: 10000
       },
       {
         brand: 'Renault',
-        model: 'macan'
+        model: 'macan',
+        price: 20000
       },
       {
         brand: 'ferarri',
-        model: '458'
+        model: '458',
+        price: 30000
       },
       {
         brand: 'lamborghini',
-        model: 'urus'
+        model: 'urus',
+        price: 40000
       }
     ];  
     interval$;
     timer$;
 
-    /*
-    // in the service
-getVehicles(){
-    return Observable.interval(2200).map(i=> [{name: 'car 1'},{name: 'car 2'}])
-}
+    // /!\ of vs from => 1- use values with no conversion / from use many types ...
+    //                   2- of will send all at once , from will send one by one    
+    carsOf$ = of(this.cars);  
+    carsFrom$ = from(this.cars);
 
-// in the controller
-vehicles: Observable<Array<any>>
-ngOnInit() {
-    this.vehicles = this._vehicleService.getVehicles();
-}
-
-// in template
-<div *ngFor='let vehicle of vehicles | async'>
-    {{vehicle.name}}
-</div>
-*/
-
+   
     ngOnInit() {    
       this.interval$ = interval(1000).pipe(take(4)).pipe(map(i=> [{name: this.cars[i].brand}]));
       this.timer$ = timer(0, 1000).pipe(map(i=> [{name: 'car 1'},{name: 'car 2'},{name: 'car 3'}]));
-      // 
-      this.helloWorldRxJs();
-      this.displyATimerEvenEvery600msOnly10Times();
-      this.useDelay();
-      this.useDelayWithFrom();
+      
+      // this.simpleDisplayWithOf();
+      // this.simpleDisplayWithOfOnArray();
+      // this.simpleDisplayWithFrom();
+      // this.helloWorldRxJs();
+      // this.displyATimerEvenEvery600msOnly10Times();
+      // this.useDelayWithOf();
+      // this.useDelayWithFrom();
+      // this.displayCars();
+      // this.getCarDataWithMap();
+      // this.getCarDataWithMergeAll();
+      this.getCarDataWithMergeMap();
+    }
+
+    simpleDisplayWithOf() {
+      console.log("=======simpleDisplayWithOf========");
+      of(1,2,3,4).pipe(map(d => d)).subscribe(console.log);
+    }
+
+    simpleDisplayWithOfOnArray() {
+      console.log("=======simpleDisplayWithOfOnArray========");
+      of([1,2,3,4]).pipe(map(d => d)).subscribe(console.log);
+    }
+    
+    simpleDisplayWithFrom() {
+      console.log("=======simpleDisplayWithFrom========");
+      from([1,2,3,4]).subscribe(console.log);
     }
 
     helloWorldRxJs() { 
@@ -63,16 +83,43 @@ ngOnInit() {
       ); 
     }
 
-    useDelay() {
-      of(1,2,3).pipe(delay(10000)).subscribe(t => console.log('Delay of 10000 is up ! '));
+    useDelayWithOf() {
+      of(1,2,3).pipe(delay(2000)).subscribe(t => console.log('Delay of Of - 2000 is up ! ', t));
     }
 
     useDelayWithFrom() {
-      from([1,2,3]).pipe(delay(13000)).subscribe(t => console.log('Delay of 13000 is up ! '));
+      from([11,22,33]).pipe(delay(2000)).subscribe(t => console.log('Delay of From - 2000 is up ! ==> ', t));
     }
 
     displyATimerEvenEvery600msOnly10Times() {
       timer(0, 600).pipe(take(10)).subscribe((t) =>  
         console.log('Example 2 : ', t));
+    }
+
+    displayCars() {
+      console.log("=======displayCars========");
+      this.carsFrom$.subscribe(console.log);
+    }
+
+    getDataFromServer(q: Car) {
+      console.log("=======getDataFromServer========", q);
+      return from([q.brand + ' ' + q.price + ' | ~(1)'
+                 , q.brand + ' ' + q.price + ' | ~(2)'])
+                 // .pipe(delay(1000));
+    }
+
+    getCarDataWithMap() {
+      this.carsFrom$.pipe(map(c => this.getDataFromServer(c)))
+                    .subscribe(r => r.subscribe(console.log));
+    }
+
+    getCarDataWithMergeAll() {
+      this.carsFrom$.pipe(map(c => this.getDataFromServer(c)), mergeAll())                    
+                    .subscribe((console.log));
+    }
+
+    getCarDataWithMergeMap() {
+      this.carsFrom$.pipe(mergeMap(c => this.getDataFromServer(c)))                                           
+                    .subscribe((console.log));
     }
 }
