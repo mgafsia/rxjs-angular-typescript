cb3050399ef7ea1d76ddba84283cf042a78cce32
mgafsia <mohamed.gafsia@gmail.com>
2020-03-24 02:03:47 +0100

    Add comment

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { of, from, timer, interval } from 'rxjs';
-import { map, take, delay, mergeAll, switchMap, mergeMap } from 'rxjs/operators';
+import { map, take, delay, mergeAll, switchMap, mergeMap, concatMap } from 'rxjs/operators';
 
 export interface Car {
   brand: string;
@@ -14,6 +14,22 @@ export interface Car {
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent implements OnInit  {  
+/*
+    1- of vs from => a- use values with no conversion / from use many types ...
+                       b- of will send the value of array all at once  / from will send tha array value one by one    
+
+    2- a- Delay vs interval : most siller , delay can be a 0 for the first ?? ...  
+       b- timer ? : it is a observable to give a even on each period ...    
+
+    3- map vs mergeAll vs mergeMap vs switchMap vs concatMap
+       a- map to manipulate n to (n modified)                   
+       b- mergeAll and mergeMap will merge all observable subscripion on one (we don't care about order - ill will be flat)
+       c- switch map will switch between observable subscrition but start by the last of the first one, so can lost others (If we don"t need them, that will be ok)
+       d- concatMap will preserve the order of first subscription and the second one (one from first follwed by one from the second)  
+*/
+
+
+
     cars: Car[] = [
       {
         brand: 'porsche',
@@ -39,8 +55,7 @@ export class AppComponent implements OnInit  {
     interval$;
     timer$;
 
-    // /!\ of vs from => 1- use values with no conversion / from use many types ...
-    //                   2- of will send all at once , from will send one by one    
+
     carsOf$ = of(this.cars);  
     carsFrom$ = from(this.cars);
 
@@ -59,7 +74,9 @@ export class AppComponent implements OnInit  {
       // this.displayCars();
       // this.getCarDataWithMap();
       // this.getCarDataWithMergeAll();
-      this.getCarDataWithMergeMap();
+      // this.getCarDataWithMergeMap();
+      // this.getCarDataWithSwitchMap();
+      this.getCarDataWithConcatMap();
     }
 
     simpleDisplayWithOf() {
@@ -105,7 +122,7 @@ export class AppComponent implements OnInit  {
       console.log("=======getDataFromServer========", q);
       return from([q.brand + ' ' + q.price + ' | ~(1)'
                  , q.brand + ' ' + q.price + ' | ~(2)'])
-                 // .pipe(delay(1000));
+                 .pipe(delay(1000));
     }
 
     getCarDataWithMap() {
@@ -120,6 +137,16 @@ export class AppComponent implements OnInit  {
 
     getCarDataWithMergeMap() {
       this.carsFrom$.pipe(mergeMap(c => this.getDataFromServer(c)))                                           
+                    .subscribe((console.log));
+    }
+
+    getCarDataWithSwitchMap() { // Fait attention au delay dans getDataFromServer ==> ce n'est pas le meme resulat avec delay et sans 
+      this.carsFrom$.pipe(switchMap(c => this.getDataFromServer(c)))                                           
+                    .subscribe((console.log));
+    }
+
+     getCarDataWithConcatMap() { // Fait attention au delay dans getDataFromServer ==> ce n'est pas le meme resulat avec delay et sans 
+      this.carsFrom$.pipe(concatMap(c => this.getDataFromServer(c)))                                           
                     .subscribe((console.log));
     }
 }
